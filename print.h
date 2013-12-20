5e08c0f92cefb7d5ce43c61a50c9ff16cab74f36
Aleksandr Dobkin <adob321@gmail.com>
2013-12-19 23:21:52 -0800

    removed clashing definition; replaced with header file

--- print.h
+++ print.h
@@ -21,6 +21,7 @@
 #pragma once
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 #ifdef __GXX_RTTI
 #include <typeinfo>
@@ -29,7 +30,6 @@ char*
 __cxa_demangle(const char* __mangled_name, char* __output_buffer,
                size_t* __length, int* __status);
 
-void free(void*);
 }
 #endif
 
